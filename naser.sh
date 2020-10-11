2f4c7bb27a8cc49e887b7262dad10d038b0c27dd
naser2877 <61701313+naser2877@users.noreply.github.com>
2020-10-11 01:02:03 +0000

    Add files via upload
    hello 
    
    This program is made for those who ,Like me .Do note have the patience to install programes
    
    Install THERMUX pre-requisites completely automatically wich this program without any hassle 
    
    Mor sluggish programs are waiting to be built Enjoy and be happy :*****
    
    
    
    HOW TO INSTALL:
    
    
    
    cd naser/
    
    chmod  +x naser.sh
    
    ./naser.sh
    
    
    This script includes programs such as :
    
    tor 
    
    hydra
    
    gema-system-x86_64
    
    zip
    
    unzip
    
    nmap
    
    net-tools
    
    x11-repo
    
    unstable-repo
    
    root-repo
    
    update && upgrade
    
    python 
    
    python2

---
+++ naser.sh
@@ -0,0 +1,65 @@
+echo +++++In The Name Of Allah +++++
+sleep 6
+echo +++++++++++update++++++++++++++
+
+pkg update -y 
+sleep 3
+
+echo ++++++++++upgrade++++++++++++++
+pkg install upgrade -y
+sleep3
+
+echo ++++++++++++python+++++++++++++
+pkg install python -y  
+sleep 3
+
+echo +++++++++++python2++++++++++++++
+pkg install python2 -y
+sleep 3
+
+echo +++++++++++  tor +++++++++++++++
+apt install tor -y
+sleep 3
+
+echo +++++++++++root-repo++++++++++++
+pkg install root-repo -y
+sleep 3
+
+echo +++++++++unstable-repo++++++++++
+pkg install unstable-repo
+sleep 3
+
+echo ++++++++++++x11-repo++++++++++++
+pkg install x11-repo
+sleep 3
+
+echo +++++++++++++nmap+++++++++++++++
+apt install nmap -y
+sleep 3
+
+echo +++++++++++net-tools++++++++++++
+apt install net-tools
+sleep 
+
+echo ++++++++++++hydra+++++++++++++++
+apt install hydra -y
+sleep 3
+
+echo ++++++++++++zip+++++++++++++++++
+apt install zip
+sleep 3
+
+echo ++++++++++++unzip+++++++++++++++
+apt install unzip
+sleep 3
+
+echo ++++++gema-system-x86_64++++++++
+pkg install gema-system-x86_64
+sleep 3
+
+
+
+echo ba tashakor az shoma doostan aziz montazer amade sazi badi bashid
+echo instagram= @naser_snh
+sleep 8
+echo           The End
